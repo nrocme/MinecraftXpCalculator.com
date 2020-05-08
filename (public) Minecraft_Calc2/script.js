755202f51a5fff60a464ac6884d70841868a5ffa
Chandler Corn <rochala827@gmail.com>
2020-05-08 13:54:36 -0500

    V1.0
    This is the completed version of my we app that has been cleaned up and with some comments added. The javascript has been switched from if else chains to a switch and has some minor improvements. Furthmore the table has een remodeled and a donate paypal and bitcoin section has been added as the wesite will be ad free

--- (public) Minecraft_Calc2/script.js
+++ (public) Minecraft_Calc2/script.js
@@ -1,121 +1,155 @@
-function xpNeed() {
-curr = document.getElementById("curr").value; 
-target = document.getElementById("target").value;
-tarXp=0;
-currXp=0;
-expNeed=0;
-    if (curr <= 16) {
-     currXp = (curr * curr) + (6 * curr);
-	 }
-	if (curr > 16 && curr <= 31) {
-	 currXp = (2.5 * (curr * curr)) - (40.5 * curr) + 360;
-	}
-	if (curr >= 32) {
-	currXp = (4.5 * (curr * curr)) - (162.5 * curr) + 2220;
-	}
-	
-	 if (target <= 16) {
-     tarXp = (target * target) + (6*target);
-	 }
-	if (target > 16 && curr <= 31) {
-	 tarXp =(2.5 * (target * target)) - (40.5 * target) + 360;
-	}
-	if (target >= 32) {
-	tarXp = (4.5 * (target * target)) - (162.5 * target) + 2220;
-	}
-	expNeed = tarXp - currXp;
+// returns the amount of xp needed to go from the current experience level to the target experience level using numbers                         located at https://minecraft.gamepedia.com/Experience
+function xpNeed(currLevel, targetLevel) {
+    var currXp = 0;
+    var targetXp = 0;
+    var xpNeeded = 0;
+    switch (true) {
+        case (currLevel <= 16):
+            currXp = Math.pow(currLevel, 2) + (6 * currLevel);
+            break;
+        case (currLevel <= 31):
+            currXp = (2.5 * Math.pow(currLevel, 2)) - ((40.5 * currLevel) + 360);
+            break;
+        case (currLevel > 31): 
+            currXp = (4.5 * Math.pow(currLevel, 2)) - ((162.5 * currLevel) + 2220);
+            break;
+        default:
+            alert("value error");
+            break;
+    }
+    switch (true) {
+        case (targetLevel <= 16):
+            targetXp = Math.pow(targetLevel, 2) + (6 * targetLevel);
+            break;
+        case (targetLevel <= 31):
+            targetXp = (2.5 * Math.pow(targetLevel, 2)) - ((40.5 * targetLevel) + 360);
+            break;
+        case (targetLevel > 31): 
+            targetXp = (4.5 * Math.pow(targetLevel, 2)) - ((162.5 * targetLevel) + 2220);
+            break;
+        default:
+            alert("value error");
+            break;
+    }
+    return targetXp - currXp;
 }
 
-function calc(curr, target) {
- curr = document.getElementById("curr").value; 
- target = document.getElementById("target").value;
-  xpNeed();
+    
+function calc() {
+    var currLevel = document.getElementById("currLevel").value; 
+    var targetLevel = document.getElementById("targetLevel").value;
+    var targetXp = xpNeed(document.getElementById("currLevel").value, document.getElementById("targetLevel").value);
+    
 	var five = document.getElementsByClassName('five');
-		for (var i = 0; i < five.length; i++) {
-			five[i].innerHTML = (expNeed/5).toFixed(0);
-		}
+    for (var i = 0; i < five.length; i++) 
+        five[i].innerHTML = (targetXp/5).toFixed(0);
 		
-	var oneTo3 = document.getElementsByClassName('oneTo3');
-		for (var i = 0; i < oneTo3.length; i++) {
-			oneTo3[i].innerHTML = (expNeed/1).toFixed(0)  + " to " + (expNeed/3).toFixed(0);
-		}
-		
+    var onetotwo = document.getElementsByClassName('onetotwo');
+    for (var i = 0; i < onetotwo.length; i++) 
+        onetotwo[i].innerHTML = (targetXp/1).toFixed(0) + " to " + (targetXp/2).toFixed(0);
+    
+    var onetothree = document.getElementsByClassName('onetothree');
+    for (var i = 0; i < onetothree.length; i++)
+        onetothree[i].innerHTML = (targetXp/1).toFixed(0) + " to " + (targetXp/3).toFixed(0);
+
 	var four = document.getElementsByClassName('four');
-		for (var i = 0; i < four.length; i++) {
-			four[i].innerHTML = (expNeed/4).toFixed(0);
-		}
+    for (var i = 0; i < four.length; i++) 
+        four[i].innerHTML = (targetXp/4).toFixed(0);
+    
+	var twentytwo = document.getElementsByClassName('twentytwo');
+    for (var i = 0; i < twentytwo.length; i++) 
+        twentytwo[i].innerHTML = (targetXp/22).toFixed(0);
+    
+	var twenty = document.getElementsByClassName('twenty');
+    for (var i = 0; i < twenty.length; i++) 
+        twenty[i].innerHTML = (targetXp/20).toFixed(0);
+    
 	var three = document.getElementsByClassName('three');
-		for (var i = 0; i < three.length; i++) {
-			three[i].innerHTML =(expNeed/3).toFixed(0);
-		}
+		for (var i = 0; i < three.length; i++) 
+			three[i].innerHTML =(targetXp/3).toFixed(0);
+        
 	var two = document.getElementsByClassName('two');
-		for (var i = 0; i < two.length; i++) {
-			two[i].innerHTML = (expNeed/2).toFixed(0);
-		}
+		for (var i = 0; i < two.length; i++) 
+			two[i].innerHTML = (targetXp/2).toFixed(0);
+        
 	var twelve = document.getElementsByClassName('twelve');
-		for (var i = 0; i < twelve.length; i++) {
-			twelve[i].innerHTML = (expNeed/12).toFixed(0);
-		}
+		for (var i = 0; i < twelve.length; i++)
+			twelve[i].innerHTML = (targetXp/12).toFixed(0);
+        
 	var ten = document.getElementsByClassName('ten');
-		for (var i = 0; i < ten.length; i++) {
-			ten[i].innerHTML = (expNeed/12).toFixed(0);
-		}
-	var enderDragon = document.getElementsByClassName('enderDragon');
-		for (var i = 0; i < enderDragon.length; i++) {
-		if (expNeed < 12000) enderDragon[i].innerHTML = "1";
-		else enderDragon[i].innerHTML = (expNeed/12000).toFixed(0);
-		}
-	var oneTo2 = document.getElementsByClassName('oneTo2');
-		for (var i = 0; i < oneTo2.length; i++) {
-			oneTo2[i].innerHTML = (expNeed/1).toFixed(0) + " to " + (expNeed/2).toFixed(0);
-		}
+		for (var i = 0; i < ten.length; i++) 
+			ten[i].innerHTML = (targetXp/10).toFixed(0);
+        
+	var onetotwo = document.getElementsByClassName('onetotwo');
+		for (var i = 0; i < onetotwo.length; i++)
+			onetotwo[i].innerHTML = (targetXp/1).toFixed(0) + " to " + (targetXp/2).toFixed(0);
+    
 	var one = document.getElementsByClassName('one');
-		for (var i = 0; i < one.length; i++) {
-			one[i].innerHTML = (expNeed/1).toFixed(0);
-		}
+		for (var i = 0; i < one.length; i++)
+			one[i].innerHTML = (targetXp/1).toFixed(0);
+        
+    // special case for enderdragon
+    if (targetXp < 12000) document.getElementById('enderdragon').innerHTML = "1";
+    else document.getElementById('enderdragon').innerHTML = (targetXp/12000).toFixed(0);
+    // special case for wither
+    if (targetXp < 50) document.getElementById('wither').innerHTML = "1";
+    else document.getElementById('wither').innerHTML = (targetXp/50).toFixed(0);
+
+	var threetoseven = document.getElementsByClassName('threetoseven');
+        for (var i = 0; i < threetoseven.length; i++)
+            threetoseven[i].innerHTML = (targetXp/3).toFixed(0) + " to " + (targetXp/7).toFixed(0);
+
+	var twotofive = document.getElementsByClassName('twotofive');
+		for (var i = 0; i < twotofive.length; i++)
+			twotofive[i].innerHTML = (targetXp/2).toFixed(0) + " to " + (targetXp/5).toFixed(0);
+        
+	var onetofive = document.getElementsByClassName('onetofive');
+		for (var i = 0; i < onetofive.length; i++)
+			onetofive[i].innerHTML = (targetXp/1).toFixed(0) + " to " + (targetXp/5).toFixed(0);
+        
+	var fifteentofortythree = document.getElementsByClassName('fifteentofortythree');
+		for (var i = 0; i < fifteentofortythree.length; i++)
+			fifteentofortythree[i].innerHTML = (targetXp/15).toFixed(0) + " to " + (targetXp/43).toFixed(0);
 		
-	var fifty = document.getElementsByClassName('fifty');
-		for (var i = 0; i < fifty.length; i++) {
-			fifty[i].innerHTML = (expNeed/50).toFixed(0);
-		}
-	var threeTo7 = document.getElementsByClassName('threeTo7');
-		for (var i = 0; i < threeTo7.length; i++) {
-			threeTo7[i].innerHTML = (expNeed/3).toFixed(0) + " to " + (expNeed/7).toFixed(0);
-		}
-	var twoTo5 = document.getElementsByClassName('twoTo5');
-		for (var i = 0; i < twoTo5.length; i++) {
-			twoTo5[i].innerHTML = (expNeed/2).toFixed(0) + " to " + (expNeed/5).toFixed(0);
-		}
-	var oneTo5 = document.getElementsByClassName('oneTo5');
-		for (var i = 0; i < oneTo5.length; i++) {
-			oneTo5[i].innerHTML = (expNeed/1).toFixed(0) + " to " + (expNeed/5).toFixed(0);
-		}
-	var fifteenTo43 = document.getElementsByClassName('fifteenTo43');
-		for (var i = 0; i < fifteenTo43.length; i++) {
-			fifteenTo43[i].innerHTML = (expNeed/15).toFixed(0) + " to " + (expNeed/43).toFixed(0);
-		}
-	var zero7 = document.getElementsByClassName('zero7');
-		for (var i = 0; i < zero7.length; i++) {
-			zero7[i].innerHTML = (expNeed/0.7).toFixed(0);
-		}
-	var zero35 = document.getElementsByClassName('zero35');
-		for (var i = 0; i < zero35.length; i++) {
-			zero35[i].innerHTML = (expNeed/0.7).toFixed(0);
-		}
-	var threeTo11 = document.getElementsByClassName('threeTo11');
-		for (var i = 0; i < threeTo11.length; i++) {
-			threeTo11[i].innerHTML = (expNeed/3).toFixed(0) + " to " + (expNeed/11).toFixed(0);
-		}
-	var threeTo6 = document.getElementsByClassName('threeTo6');
-		for (var i = 0; i < threeTo6.length; i++) {
-			threeTo6[i].innerHTML = (expNeed/3).toFixed(0) + " to " + (expNeed/6).toFixed(0);
-		}
-	var oneTo7 = document.getElementsByClassName('oneTo7');
-		for (var i = 0; i < oneTo7.length; i++) {
-			oneTo7[i].innerHTML = (expNeed/1).toFixed(0) + " to " + (expNeed/7).toFixed(0);
-		}
-	var oneTo6 = document.getElementsByClassName('oneTo6');
-		for (var i = 0; i < oneTo6.length; i++) {
-			oneTo6[i].innerHTML = (expNeed/1).toFixed(0) + " to " + (expNeed/6).toFixed(0);
-		}
+	var pointseven = document.getElementsByClassName('pointseven');
+		for (var i = 0; i < pointseven.length; i++)
+			pointseven[i].innerHTML = (targetXp/0.7).toFixed(0);
+        
+	var pointthreefive = document.getElementsByClassName('pointthreefive');
+		for (var i = 0; i < pointthreefive.length; i++)
+			pointthreefive[i].innerHTML = (targetXp/0.35).toFixed(0);
+        
+	var pointtwo = document.getElementsByClassName('pointtwo');
+		for (var i = 0; i < pointthreefive.length; i++)
+			pointtwo[i].innerHTML = (targetXp/0.2).toFixed(0);
+        
+	var pointonefive = document.getElementsByClassName('pointonefive');
+		for (var i = 0; i < pointthreefive.length; i++)
+			pointonefive[i].innerHTML = (targetXp/0.15).toFixed(0);
+        
+	var pointone = document.getElementsByClassName('pointone');
+		for (var i = 0; i < pointthreefive.length; i++)
+			pointone[i].innerHTML = (targetXp/0.1).toFixed(0);
+        
+	var threetoeleven = document.getElementsByClassName('threetoeleven');
+		for (var i = 0; i < threetoeleven.length; i++)
+			threetoeleven[i].innerHTML = (targetXp/3).toFixed(0) + " to " + (targetXp/11).toFixed(0);
+        
+	var threetosix = document.getElementsByClassName('threetosix');
+		for (var i = 0; i < threetosix.length; i++)
+			threetosix[i].innerHTML = (targetXp/3).toFixed(0) + " to " + (targetXp/6).toFixed(0);
+        
+	var onetoseven = document.getElementsByClassName('onetoseven');
+		for (var i = 0; i < onetoseven.length; i++)
+			onetoseven[i].innerHTML = (targetXp/1).toFixed(0) + " to " + (targetXp/7).toFixed(0);
+        
+	var onetosix = document.getElementsByClassName('onetosix');
+		for (var i = 0; i < onetosix.length; i++)
+			onetosix[i].innerHTML = (targetXp/1).toFixed(0) + " to " + (targetXp/6).toFixed(0);
+        
+	var zerototwo = document.getElementsByClassName('zerototwo');
+		for (var i = 0; i < zerototwo.length; i++)
+			zerototwo[i].innerHTML = (targetXp/0).toFixed(0) + " to " + (targetXp/2).toFixed(0);
+        
+    
 }
